3440c64f435a4a6f2d9b2139eb224a4b412374e2
jcvvdf <jcvvdf@gmail.com>
2017-02-08 18:14:01 -0400

    Changed the title to Julio, changed the size, and adjusted the location to the semi center of a Macbook Pro Retina Screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,9 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		myFrame.setTitle("Julio");
+		myFrame.setLocation(625, 350);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
