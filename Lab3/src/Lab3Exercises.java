5ebf5c4dba10c124ef625ab097ebf23fb1c0118b
jcvvdf <jcvvdf@gmail.com>
2017-02-08 18:17:01 -0400

    Added last name to the title.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setTitle("Julio");
+		myFrame.setTitle("Julio Valdes");
 		myFrame.setLocation(625, 350);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
