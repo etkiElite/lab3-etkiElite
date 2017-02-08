2a0f1511774178f66e133a4a784dfd5f8f64a353
jcvvdf <jcvvdf@gmail.com>
2017-02-08 18:50:59 -0400

    Removed previous drawings in the panel and added a centered oval.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -20,15 +20,25 @@ public class MyPanelClass extends JPanel {
                         int y2 = getHeight() - myInsets.bottom - 1;
                         int width = x2 - x1;
                         int height = y2 - y1;
+                        int centerx = width/2;
+                        int centery = height/2;
  
                         //Paint the background
-                        g.setColor(Color.LIGHT_GRAY);
+                        g.setColor(Color.DARK_GRAY);
                         g.fillRect(x1, y1, width + 1, height + 1);
-                        //Draw a border
-                        g.setColor(Color.YELLOW);
-                        g.drawRect(x1, y1, width, height);
-                        //Draw a second border
-                        g.setColor(Color.RED);
-                        g.drawRect(x1+4, y1+4, width-8, height-8);
+//                        //Draw a border
+//                        g.setColor(Color.YELLOW);
+//                        g.drawRect(x1, y1, width, height);
+//                        //Draw a second border
+//                        g.setColor(Color.RED);
+//                        g.drawRect(x1+4, y1+4, width-8, height-8);
+//                        //Draw lines
+//                        g.setColor(Color.WHITE);
+//                        g.drawLine(x1, y1, x2, y2);
+//                        g.setColor(Color.GREEN);
+//                        g.drawLine(x1, y2, x2, y1);
+                        //Draw an Oval
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillOval(centerx-27, centery-27, 55, 55);
             }
 }
